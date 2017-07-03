64e0adc903684e7946f2e599eb25268ba8021576
Chris Fischer <chris@fischer.nyc>
2017-07-03 01:03:01 -0400

    added in datetime calcuation

--- aaptiv/data-analysis/data_runner.py
+++ aaptiv/data-analysis/data_runner.py
@@ -17,23 +17,20 @@ Unique_Days_Classes_Taken_Per_User_Per_Month_Post_Feb_1_2017.csv
 import numpy as np
 import pandas as p
 
-def list_comp(list1, list2):
-    count = 0
-    row = 0
-    for val in list1:
-        row = row + 1
-        if val not in list2:
-            count = count + 1
-            print(count, row)
-    return count
-
 total_class_details = p.DataFrame.from_csv('data_files/Month_by_Month_Total_Workout_Detail_Views.csv')
 unique_class_details = p.DataFrame.from_csv('data_files/Month_by_Month_Unique_Days_Viewed_Workout_List.csv')
 subscribers = p.DataFrame.from_csv('data_files/Subscribers_with_Dates_Created_Trial_Start_First_Pay_Expiration.csv')
 
 # print(p.to_datetime(subscribers['account_created_at']))
 
-print(unique_class_details.quantile([.1, .2, .3, .4, .5, .6, .7, .8, .9], axis=0))
+# print(unique_class_details.quantile([.1, .2, .3, .4, .5, .6, .7, .8, .9], axis=0))
+
+subscribers['subscription_expiration_date'] = p.to_datetime(subscribers['subscription_expiration_date'])
+subscribers['first_payment_date'] = p.to_datetime(subscribers['first_payment_date'])
+subscribers['months'] = subscribers['subscription_expiration_date'] - subscribers['first_payment_date']
+
+print(subscribers['months'])
+
 
 '''
 f = month_on_month
