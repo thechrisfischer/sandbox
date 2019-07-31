740360ae4afedbceaa7a89621163c1b06d0d6d13
Chris Fischer <chris@fischer.nyc>
2019-07-31 17:23:29 -0400

    working on way to show multiple ips not just the first one

--- scripts/slack_status.sh
+++ scripts/slack_status.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
 function check_ip {
-    local ip=$1
+    args="$*"
     local iparray=()
-    if [ `echo $ip | grep -o '\.' | wc -l` -eq 3 ] && [ `echo $ip | tr '.' ' ' | wc -w` -eq 4 ]; then
-        iparray+=($ip)
-    else
-        exit 1
-#            for OCTET in echo $1 | tr '.' ' '; do
-#                    if ! [[ $OCTET =~ ^[0-9]+$ ]]; then
-#                            echo "Parameter '$1' does not look like in IP Address (octet '$OCTET' is not numeric).";
-#                            exit 1;
-#                    elif [[ $OCTET -lt 0 || $OCTET -gt 255 ]]; then
-#                            echo "Parameter '$1' does not look like in IP Address (octet '$OCTET' in not in range 0-255).";
-#                            exit 1;
-#                    fi
-#            done
-    fi
+    for i in $args
+    do
+        if [ `echo $i | grep -o '\.' | wc -l` -eq 3 ] && [ `echo $i | tr '.' ' ' | wc -w` -eq 4 ]; then
+            iparray+=($i)
+        else
+            exit 1
+#                for OCTET in echo $1 | tr '.' ' '; do
+#                        if ! [[ $OCTET =~ ^[0-9]+$ ]]; then
+#                                echo "Parameter '$1' does not look like in IP Address (octet '$OCTET' is not numeric).";
+#                                exit 1;
+#                        elif [[ $OCTET -lt 0 || $OCTET -gt 255 ]]; then
+#                                echo "Parameter '$1' does not look like in IP Address (octet '$OCTET' in not in range 0-255).";
+#                                exit 1;
+#                        fi
+#                done
+        fi
+    done
     echo ${iparray[*]}
 }
 
 
 function get_ip {
-    local ip_address=`ifconfig | grep inet | awk {'print $2'}`
+    local ip_address=`ifconfig | grep inet | awk {'print $2'} | tr '\n' ' '`
     local value=$(check_ip $ip_address)
     echo $value
 }
@@ -32,7 +35,9 @@ function post_slack {
     local ip=$(get_ip)
     local slack_url="https://hooks.slack.com/services/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"
     local payload="{\"text\":\"$ip\"}"
-    curl -X POST -H "Content-type: application/json" --data "$payload" $slack_url
+    echo $payload
+    #curl -X POST -H "Content-type: application/json" --data "$payload" $slack_url
 }
 
 post_slack
+echo $args
