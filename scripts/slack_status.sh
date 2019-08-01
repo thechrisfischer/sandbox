b2402bde1026141c12b7a2215903858eb37715a1
Chris Fischer <chris@fischer.nyc>
2019-08-01 14:36:51 -0400

    script now passing outputing all ip addresses

--- scripts/slack_status.sh
+++ scripts/slack_status.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 function check_ip {
-    args="$*"
     local iparray=()
-    for i in $args
+    for i in "$@"
     do
-        if [ `echo $i | grep -o '\.' | wc -l` -eq 3 ] && [ `echo $i | tr '.' ' ' | wc -w` -eq 4 ]; then
+        if [ $(echo $i | grep -o '\.' | wc -l) -eq 3 ] && [ $(echo $i | tr '.' ' ' | wc -w) -eq 4 ]; then
             iparray+=($i)
-        else
-            exit 1
 #                for OCTET in echo $1 | tr '.' ' '; do
 #                        if ! [[ $OCTET =~ ^[0-9]+$ ]]; then
 #                                echo "Parameter '$1' does not look like in IP Address (octet '$OCTET' is not numeric).";
@@ -20,14 +17,15 @@ function check_ip {
 #                done
         fi
     done
-    echo ${iparray[*]}
+    local output=${iparray[*]}
+    echo $output
 }
 
 
 function get_ip {
     local ip_address=`ifconfig | grep inet | awk {'print $2'} | tr '\n' ' '`
-    local value=$(check_ip $ip_address)
-    echo $value
+    local output=$(check_ip $ip_address)
+    echo $output
 }
 
 
@@ -35,9 +33,7 @@ function post_slack {
     local ip=$(get_ip)
     local slack_url="https://hooks.slack.com/services/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"
     local payload="{\"text\":\"$ip\"}"
-    echo $payload
-    #curl -X POST -H "Content-type: application/json" --data "$payload" $slack_url
+    curl -X POST -H "Content-type: application/json" --data "$payload" $slack_url
 }
 
 post_slack
-echo $args
